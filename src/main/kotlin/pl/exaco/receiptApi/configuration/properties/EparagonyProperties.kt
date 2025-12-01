b29d450d5132977aeba5f5d73255e1334597bba1
Maciej Szczepaczyk <maciej.szczepanczyk@exaco.pl>
2025-12-01 10:47:23 +0100

    appconfig fix

--- src/main/kotlin/pl/exaco/receiptApi/configuration/properties/EparagonyProperties.kt
+++ src/main/kotlin/pl/exaco/receiptApi/configuration/properties/EparagonyProperties.kt
@@ -1,7 +1,9 @@
 package pl.exaco.receiptApi.configuration.properties
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 class EparagonyProperties(
     val api: Api
 )
